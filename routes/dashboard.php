6c56c95cffd073e00347e9d7e2c6640bf8adffc3
Onur SEDEF <onrsedef@gmail.com>
2022-08-04 16:17:40 +0300

    Routes updated.

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 Route::middleware(['auth'])->group(function() {
     Route::get('/dashboard', function() {
-        return view('welcome');
+        return view('dashboard.index');
     })->name('home');
 });
