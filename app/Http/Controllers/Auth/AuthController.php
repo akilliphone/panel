62a40407dd5033feb23ea690a9bef89dac1c7bff
Onur SEDEF <onrsedef@gmail.com>
2022-08-05 16:22:47 +0300

    Login Page Designed & Login Test Added

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
         if (Auth::attempt($check)) {
             return redirect()->route('home');
         }
-        return redirect()->route('login')->withErrors(['email' => 'Invalid credentials']);
+        return redirect()->route('login')->withErrors(['message' => 'Invalid credentials']);
     }
 
     public function register(Request $request)
