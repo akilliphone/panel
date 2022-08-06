b0d46e69e611fefac0d7e295401385d9c742228f
Onur SEDEF <onrsedef@gmail.com>
2022-08-06 12:47:35 +0300

    Jquery Datatables files added to local.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,12 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel - Dashboard</title>
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
     <script src="{{ asset('assets/js/jquery.js') }}"></script>
-    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('assets/css/jquery.dataTables.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/buttons.dataTables.css') }}">
+
+    <script type="text/javascript" src="{{ asset('assets/js/jquery.dataTables.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/dataTables.buttons.js') }}"></script>
 </head>
 <body>
 <nav>
